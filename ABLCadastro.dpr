45eb5e7d017f37229f1821083f12a8cdedde1f86
Adriano-bertellini <135441385+Adriano-bertellini@users.noreply.github.com>
2025-11-18 23:54:33 -0300

    Inicilização do projeto - ABL Cadastro

---
+++ ABLCadastro.dpr
@@ -0,0 +1,15 @@
+program ABLCadastro;
+
+uses
+  Vcl.Forms,
+  uFormMain in 'uFormMain.pas' {FormMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFormMain, FormMain);
+  Application.CreateForm(TFormMain, FormMain);
+  Application.Run;
+end.
