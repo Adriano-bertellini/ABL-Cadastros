26edb95dd173c353fdfcd637e0eed71dfeb54fe3
Adriano-bertellini <135441385+Adriano-bertellini@users.noreply.github.com>
2025-11-20 20:10:19 -0300

    Criação da Tela de Login e validação do banco de dados.

--- uFormMain.pas
+++ uFormMain.pas
@@ -7,11 +7,14 @@ uses
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, FireDAC.Stan.Intf, FireDAC.Stan.Option,
   FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
   FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.FB,
-  FireDAC.Phys.FBDef, FireDAC.VCLUI.Wait, Data.DB, FireDAC.Comp.Client;
+  FireDAC.Phys.FBDef, FireDAC.VCLUI.Wait, Data.DB, FireDAC.Comp.Client,
+  Vcl.Imaging.pngimage, Vcl.ExtCtrls, Vcl.Buttons, Vcl.StdCtrls;
 
 type
   TFormMain = class(TForm)
     Conexao: TFDConnection;
+    Panel1: TPanel;
+    imgLogo: TImage;
   private
     { Private declarations }
   public
@@ -20,9 +23,10 @@ type
 
 var
   FormMain: TFormMain;
-
-
 implementation
+
+uses
+  uLogin;
 
 {$R *.dfm}
 
