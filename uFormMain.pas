fbbe2bd5995b8bf468bc0e8d74200d29c71c3c12
Adriano-bertellini <135441385+Adriano-bertellini@users.noreply.github.com>
2025-11-20 18:08:20 -0300

    Conexão e criação do banco de dados - FB

--- uFormMain.pas
+++ uFormMain.pas
@@ -4,10 +4,14 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.FB,
+  FireDAC.Phys.FBDef, FireDAC.VCLUI.Wait, Data.DB, FireDAC.Comp.Client;
 
 type
   TFormMain = class(TForm)
+    Conexao: TFDConnection;
   private
     { Private declarations }
   public
